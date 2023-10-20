9c5760f4b98eaa2336690a8e07fbc12da3aca982
LUCAS050423 <145132132+LUCAS050423@users.noreply.github.com>
2023-10-20 10:39:50 -0300

    Add files via upload

---
+++ aoQuadrado.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+public class aoQuadrado {	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		final int TAM = 10; 
+		int a[], b[], i;
+		a = new int[TAM];
+		b = new int[TAM];
+		
+		for(i=0; i<TAM; i++) {
+			System.out.println("o "+(i+1)+" valor de A");
+			a[i]= in.nextInt();
+	
+		b[i] = a[i] * a[i];
+		}
+		
+		
+		
+		System.out.print("\nb = ");
+
+		for(i=0; i<TAM; i++) {
+			System.out.print(b[i]+" ");
+		} 
+		in.close();
+
+	}
+
+}
